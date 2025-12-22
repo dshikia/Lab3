a8b0dbbdf703c297f7e791e3fd6ca199316781b7
dshikia <145121772+dshikia@users.noreply.github.com>
2025-12-22 18:51:34 +0300

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,155 +1,45 @@
-class Keyboard {
-    private boolean[] bukvi;
-    private boolean[] zifri;
-    private boolean shift;
+public class Test {
+    public static void main(String[] args) {
+        Keyboard k = new Keyboard();
 
-    public Keyboard() {
-        bukvi = new boolean[26];
-        zifri = new boolean[10];
-        shift = true;
+        System.out.println("1. Начальные сломанные клавиши: " + k.BrokenKeys());
 
-        for (int i = 0; i<26; i++)
-            bukvi[i] = true;
-        for (int i=0; i<10; i++)
-            zifri[i] = true;
+        k.Breakk('*');
+        k.Breakk('a');
+        k.Breakk('1');
+        System.out.println("2. После поломки SHIFT, a, 1: " + k.BrokenKeys());
+
+        System.out.println("3. Можно напечатать 'a'? " + k.Printt('a'));
+        System.out.println("Можно напечатать 'B'?" + k.Printt('A'));
+        System.out.println("Можно напечатать '1'?" + k.Printt('1'));
+        System.out.println("Можно напечатать 'b'?" + k.Printt('b'));
+
+        k.Repair('a');
+        System.out.println("4. После восстановления 'a': " + k.BrokenKeys());
+
+        System.out.println("5. Можно напечатать 'apple'? " + k.PrintWord("apple"));
+        System.out.println("Можно напечатать 'Apple'? " + k.PrintWord("Apple"));
+        System.out.println("Можно напечатать 'Cat'? " + k.PrintWord("Cat"));
+
+        System.out.println("6. Есть сломанные буквы? " + k.Bukvibr());
+
+        System.out.println("7. Все цифры сломаны? " + k.ZifriAllBr());
+
+        System.out.println("8. Для слова 'Cat123': " + k.KlWord("Cat123"));
+
+        String text = "Three cats Watched 1 apple fall";
+        System.out.println("9. Можно напечатать слов из '" + text + "': " + k.WordText(text));
+
+        System.out.println("10. Слово 'Apple123':");
+        k.AnWord("Apple123");
+
+        System.out.println("11. Восстанавливаем 'a1*':");
+        k.BosstanPolomka("a1*", true);
+        System.out.println("После восстановления: " + k.BrokenKeys());
+
+        System.out.println("Ломаем 'r7w':");
+        k.BosstanPolomka("r7w", false);
+        System.out.println("После поломки: " + k.BrokenKeys());
     }
-
-    public String BrokenKeys() {
-        String r="";
-        for (int i = 0; i <26; i++) {
-            if (bukvi[i]==false)
-                r+=(char) ('a' + i) + ", ";
-        }
-        for (int i = 0; i < 10; i++) {
-            if (zifri[i]==false)
-                r += (char) ('0' + i) + ", ";
-        }
-        if (shift==false)
-            r+= "SHIFT,";
-        if (r.length() == 0)
-            return "Все клавиши работают";
-        return r;
-    }
-
-    public void Breakk(char x) {
-        if (x >='a' && x <='z')
-            bukvi[x-'a'] = false;
-        else if (x>= 'A' && x <= 'Z')
-            bukvi[x-'A'] = false;
-        else if (x >= '0' && x <= '9')
-            zifri[x-'0'] = false;
-        else if (x == '*')
-            shift = false;
-    }
-
-    public boolean Printt(char x) {
-        if (x >= 'a' && x <= 'z')
-            return bukvi[x - 'a'];
-        else if (x >= 'A' && x <= 'Z') {
-            if (shift == false)
-                return false;
-            else if (bukvi[x -'A'] == false)
-                return false;
-        return true;
-        }
-        else if (x >= '0' && x <= '9')
-            return zifri[x-'0'];
-        return true;
-    }
-
-    public void Repair(char x) {
-        if (x >= 'a' && x <= 'z')
-            bukvi[x - 'a'] = true;
-        else if (x >='A' && x <= 'Z')
-            bukvi[x-'A'] = true;
-        else if (x >= '0' && x <= '9')
-            zifri[x - '0'] = true;
-        else if (x == '*')
-            shift = true;
-    }
-
-    public boolean PrintWord(String word) {
-        for (int i = 0; i < word.length(); i++) {
-            if (Printt(word.charAt(i))==false)
-                return false;
-        }
-        return true;
-    }
-
-    public boolean Bukvibr() {
-        for (int i = 0; i < 26; i++) {
-            if (bukvi[i]==false)
-                return true;
-        }
-        return false;
-    }
-
-    public boolean ZifriAllBr() {
-        for (int i = 0; i < 10; i++) {
-            if (zifri[i]==true)
-                return false;
-        }
-        return true;
-    }
-
-    public String KlWord(String word) {
-        String r="";
-        for (int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            if (c>='a' && c <= 'z') {
-                if (bukvi[c-'a']==false)
-                    r+=c + ", ";
-            }
-            else if (c >= 'A' && c <= 'Z') {
-                if (bukvi[c-'A']==false)
-                    r += (char)(c+32) + ", ";
-                if (shift==false)
-                    r += "SHIFT, ";
-            }
-            else if (c>='0' && c <= '9') {
-                if (zifri[c - '0']==false)
-                    r+=c + ", ";
-            }
-        }
-        if (r.length() == 0)
-            return "Все клавиши есть";
-        return r;
-    }
-
-    public int WordText(String text) {
-        String[] words = text.split(" ");
-        int k = 0;
-        for (int i = 0; i < words.length; i++) {
-            String w = words[i];
-            boolean canPrint = PrintWord(w);
-            if (canPrint == true)
-                k ++;
-        }
-        return k;
-    }
-
-    public void AnWord(String word) {
-        for (int i = 0; i<word.length();i++) {
-            char c = word.charAt(i);
-            if (Printt(c)==true)
-                System.out.print(c);
-            else {
-                Breakk(c);
-                System.out.print("[" + c + "-broken]");
-            }
-        }
-        System.out.println();
-    }
-
-    public void BosstanPolomka(String x, boolean y) {
-        for (int i = 0; i < x.length(); i++) {
-            char c = x.charAt(i);
-            if (y==true)
-                Repair(c);
-            else
-                Breakk(c);
-        }
-    }
-
 }
 
